370f8a31a75dfe8ff9acbd24a19c4d6a3094a9bd
qwe81130446 <xx6659@gmail.com>
2018-03-21 13:15:59 -0400

    update

--- test.js
+++ test.js
@@ -3,3 +3,24 @@ function myFunction(){
 	//window.location.href = "https://www.dropbox.com/s/rfu734rkrksyh1f/phoneself.exe?dl=1";
 	alert("Downloading Corn!!!")
 }
+window.onload = function(){
+	window.scrollTo(0,0)
+
+}
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 5) {
+        // you're at the bottom of the page
+        //alert("you are at the bottom of the page")
+        extendBody()
+    }
+};
+
+function extendBody(){
+	//alert(document.getElementById("bodyID").offsetHeight);
+	var constant = 20;
+	var body = document.getElementById("bodyID").offsetHeight;
+	document.getElementById("bodyID").style.height = body + constant 
+
+
+}
